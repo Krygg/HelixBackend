48cff631df7f910ef751ff8164037e24528bf6f4
Benis <tander16@student.aau.dk>
2019-05-07 10:40:11 +0200

    Go'dav'då'

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,30 @@ package com.company;
 import com.company.Drums.BassDrum;
 import com.company.Drums.Drum;
 import com.company.Drums.SnareDrum;
+import com.company.TimedJobs.Metronome;
+import com.company.TimedJobs.JobHandler;
+import com.company.TimedJobs.TestJobs;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
 
+    int id = 0;
+
 
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        //Main.testBackEnd();
+        TestJobs testJob = new TestJobs();
+        JobHandler jobHandler = new JobHandler(testJob.getJob());
+        Metronome metronome = new Metronome(128, 4);
+        metronome.startComputingOnBeat(jobHandler, metronome.getBpm());
+
+
+    }
+
+    private static void testBackEnd() throws IOException, InterruptedException {
         Synth synth3 = new Synth.Builder().Node("c4,e4").build();
         Synth synth4 = new Synth.Builder().Node("e2").build();
 
@@ -33,9 +49,6 @@ public class Main {
         instrumentSplitter.addMusicElement(drum1);
         instrumentSplitter.prepForPlay();
         instrumentSplitter.play();
-
-
     }
-
 
 }
